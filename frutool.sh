3b674c127248c07e9a05acb786ef0373d47363a1
Computer Cheese <computercheese@outlook.com>
2023-08-11 16:42:29 +0800

    Fix shellcheck issues

--- frutool.sh
+++ frutool.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+#2023-08-11 Fix shellcheck issues
 
 #2022-07-22 Stop on error
 set -e
 
-if [ -f "fru.sh" ]; then  
+if [ -f "fru.sh" ]; then
     rm fru.sh
 fi 
 
-filename='fru.conf'
-#filename='fru_conf_example.conf'
+filename=fru.conf
+#filename=fru_conf_example.conf
 
 COMM_HEAR_VALE=()
 INTL_AREA_VALE=()
@@ -30,15 +31,15 @@ function FIELD_INQUIRY() {
     local -n _FILD_NAME=$2
     local -n _FILD_VALE=$3
 
-    str=$(echo $_READ_LINE | awk -F"=" '{ print $1 }')
+    str=$(echo "$_READ_LINE" | awk -F"=" '{ print $1 }')
 
-    for ((i=0; i < ${#_FILD_NAME[@]}; i++))
+    for ((i=0; i<${#_FILD_NAME[@]}; i++))
     do
-        if [ "$str" == ${_FILD_NAME[$i]} ]; then
-            _READ_STRG=$(echo $_READ_LINE | awk -F'[=]' '$0=$2')
-            strlen=$(echo $_READ_STRG | awk '{print length}')
+        if [ "$str" == "${_FILD_NAME[$i]}" ]; then
+            _READ_STRG=$(echo "$_READ_LINE" | awk -F'[=]' '$0=$2')
+            strlen=$(echo "$_READ_STRG" | awk '{print length}')
 
-            if [ $strlen -ge 1 ] ; then
+            if [ "$strlen" -ge 1 ]; then
                 _FILD_VALE[i]=${_READ_STRG}
             fi
         fi
@@ -57,25 +58,25 @@ function AREA_INQUIRY() {
 
     strlen=$((${#arg2}))
 
-    if [ $strlen -eq 0 ] ; then
+    if [ "$strlen" -eq 0 ]; then
         TmpStr=C0
         return 0
     fi
 
-    for ((x = 0; x < $strlen ; x++))
+    for ((x=0; x<strlen ; x++))
     do
         substr=${arg2:$x:1}
         var=$(printf %d\\n "'$substr")
         #ASCII Letter Case Changed ex: a changes to A
-        #if [ "$var" -ge 97 ] && [ "$var" -le 122 ] ; then
+        #if [ "$var" -ge 97 ] && [ "$var" -le 122 ]; then
         #  var=$(($var - 32))
         #fi
 
         #Sum all values in Dec
-        sum=$(($sum+$var))
+        sum=$((sum + var))
         #Change to Hex base
-        var=`echo "obase=16; ${var}" | bc`
-        i=$(( $x + $_ARRY_START ))
+        var=$(echo "obase=16; ${var}" | bc)
+        i=$((x + _ARRY_START))
         _ARRY_SAVE[i]=$var
         TmpStr=$TmpStr$sp$var
     done
@@ -89,22 +90,22 @@ function FETCH_FIELD_DATA()
     local -n _FILD_VALE_ARRY=$3
     local -n _AREA_VALE_ARRY=$4
     
-    for (( j=$_ARRY_START ; j < ${#_FILD_NAME_ARRY[@]} ; j++))
+    for (( j=_ARRY_START; j<${#_FILD_NAME_ARRY[@]}; j++))
     do
         strlen=0
         strs="${_FILD_VALE_ARRY[j]}"
         strlen=$((${#strs}))
         #2022-04-08 Fixed issue about cannot over 15 characters
-        strlen=$(($strlen + 192))
+        strlen=$((strlen + 192))
         x=${#_AREA_VALE_ARRY[@]}
         ## code bits 7:6=11b the serial number will always be
         ## interpreted as 8-bit ASCII+Latin 1, not 16-bit UNICODE.
-        strlenHEX=`echo "obase=16; ${strlen}" | bc`
+        strlenHEX=$(echo "obase=16; ${strlen}" | bc)
         #2022-04-08 Fixed issue about cannot over 15 characters
         _AREA_VALE_ARRY[x]=$strlenHEX
 
         x=${#_AREA_VALE_ARRY[@]}
-        AREA_INQUIRY $x "${_FILD_VALE_ARRY[j]}" _AREA_VALE_ARRY
+        AREA_INQUIRY "$x" "${_FILD_VALE_ARRY[j]}" _AREA_VALE_ARRY
     done
 }
 
@@ -114,17 +115,17 @@ function ADD_ZERO_BYTE()
 
     #Write field length to area bype[1]
     x=${#_ARRY[@]}
-    area_len=$(($x / 8 + 1))
+    area_len=$((x / 8 + 1))
     #Add leading 0
     area_lenHEX=$(printf "%02X" $area_len)
     #Area Length (in multiples of 8 bytes)
     _ARRY[1]=$area_lenHEX
 
     #Write 00 to reset bytes
-    num_add_zero=$(($area_len * 8 - $x - 1))
-    for ((i=0; i<$num_add_zero; i++))
+    num_add_zero=$((area_len * 8 - x - 1))
+    for ((i=0; i<num_add_zero; i++))
     do
-        p=$(($x + $i))
+        p=$((x + i))
         _ARRY[p]=00
     done
 }
@@ -136,13 +137,13 @@ function WRITE_CHECKSUM()
     sum=0
     sumDEC=0
 
-    for ((i=0; i < x; i++))
+    for ((i=0; i<x; i++))
     do
         subHEX=${_ARRY[i]}
-        sumDEC=$(($sumDEC+$((16#$subHEX))))
+        sumDEC=$((sumDEC + $((16#$subHEX))))
     done
 
-    CheckSumDEC=$((256-($sumDEC % 256)))
+    CheckSumDEC=$((256 - (sumDEC % 256)))
     CheckSumHEX=$(printf "%02X" ${CheckSumDEC})
     _ARRY[x]=$CheckSumHEX
 }
@@ -150,7 +151,7 @@ function WRITE_CHECKSUM()
 #===============================================================================
 #Read per line from file
 exec < $filename
-while read line
+while read -r line
 do
     FIELD_INQUIRY "$line" CHAS_FILD_NAME CHAS_FILD_VALE
     FIELD_INQUIRY "$line" BOAD_FILD_NAME BOAD_FILD_VALE
@@ -207,7 +208,7 @@ mfgdtstr="${BOAD_FILD_VALE[0]}"
 #Date & Time in minutes from 1996
 st_now=$(date +%s -d '1996-01-01 00:00:00')
 dt=$(date +%s -d "$mfgdtstr")
-dtnow=$((($dt-$st_now)/60))
+dtnow=$(((dt-st_now)/60))
 datedfHEX=$(printf "%06X" ${dtnow})
 H_CK=${datedfHEX:0:2}
 M_CK=${datedfHEX:2:2}
@@ -279,13 +280,13 @@ COMM_HEAR_VALE[2]=02
 
 #Board Area Starting Offset (in multiples of 8 bytes)
 x=$((${#INTL_AREA_VALE[@]} + ${#CHAS_AREA_VALE[@]}))
-offset=$(($x / 8 + 1))
+offset=$((x / 8 + 1))
 offHEX=$(printf "%02X" ${offset})
 COMM_HEAR_VALE[3]=$offHEX
 
 #Product Info Area Starting Offset (in multiples of 8 bytes)
 x=$((${#INTL_AREA_VALE[@]} + ${#CHAS_AREA_VALE[@]} + ${#BOAD_AREA_VALE[@]}))
-offset=$(($x / 8 + 1))
+offset=$((x / 8 + 1))
 offHEX=$(printf "%02X" ${offset})
 COMM_HEAR_VALE[4]=$offHEX
 
@@ -306,9 +307,9 @@ function GET_OFFSET()
 {
     offsetadd=0
 
-    k=$(($bytecount - 8))
+    k=$((bytecount - 8))
     offsetadd=$(printf "%02X" ${k})
-    tmpoffsetstr="0x"$offsetadd" 0x00"
+    tmpoffsetstr="0x$offsetadd 0x00"
 
     export OFFSET_STRG="$tmpoffsetstr"
 }
@@ -317,18 +318,18 @@ function PRINT_IPMI()
 {
     local -n _ARRY=$1
 
-    for ((i=0; i < ${#_ARRY[@]}; i++))
+    for ((i=0; i<${#_ARRY[@]}; i++))
     do
         y=${_ARRY[i]}
-        k=$(($bytecount % 8))
+        k=$((bytecount % 8))
         bytestring=$bytestring" ""0x"$y
 
-        if [ $k -eq 0 ] ; then
+        if [ $k -eq 0 ]; then
             GET_OFFSET
-            echo $IPMI_COMD $OFFSET_STRG $bytestring >> fru.sh
+            echo "$IPMI_COMD" "$OFFSET_STRG" "$bytestring" >> fru.sh
             bytestring=""
         fi
-        bytecount=$(($bytecount + 1))
+        bytecount=$((bytecount + 1))
     done
 
 }
